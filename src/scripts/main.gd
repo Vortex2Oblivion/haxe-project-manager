38dea580e628894aa2c57b0c2cb9e3cc4c18e275
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-08-27 10:30:41 -0400

    this makes more sense

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var new_project = preload("res://src/scenes/new_project.tscn").instantiate()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +19,5 @@ func _on_button_pressed():
 	$"Import Project Button".visible = false
 	$"New Project".visible = false
 
-
 func _on_new_project_button_pressed():
-	$"Folder Open".visible = false
-	$"New Project Button".visible = false
-	$"Import Project Button".visible = false
-	$"New Project".visible = true
+	get_tree().root.add_child(new_project)
--- src/scripts/new_project.gd
+++ src/scripts/new_project.gd
@@ -18,3 +18,7 @@ func _on_add_item_button_pressed():
 
 func _on_create_pressed():
 	DirAccess.make_dir_absolute($Path.text)
+
+
+func _on_button_pressed():
+	get_tree().root.remove_child(self)
